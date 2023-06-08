3733a1fddc16f8376c7e4a5c22c6870999a5840c
Emmanuel <romero512497@gmail.com>
2023-06-08 11:12:42 -0500

    add banner and branding components and introduce constants to :root

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,15 @@
+import "./banner.css";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="banner-wrapper">
+        <div className="banner-container">
+          <h1>some announcement</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
---
+++ src/components/branding/Branding.tsx
@@ -0,0 +1,11 @@
+import "./branding.css";
+
+const Branding = () => {
+  return (
+    <>
+      <div className="branding-wrapper">Branding</div>
+    </>
+  );
+};
+
+export default Branding;
